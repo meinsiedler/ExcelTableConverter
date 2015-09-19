5fcf28edb678a6f3ecc4d7d75e795739bd341d72
Michael Einsiedler <meinsiedler@users.noreply.github.com>
2015-09-19 18:26:05 +0200

    fixed tests for CurrentCulture (also concering CI builds)

--- ExcelTableConverter.Utilities.Tests/StringExtensionsTests.cs
+++ ExcelTableConverter.Utilities.Tests/StringExtensionsTests.cs
@@ -1,4 +1,5 @@
 ﻿using System.Globalization;
+using System.Threading;
 using NUnit.Framework;
 
 namespace ExcelTableConverter.Utilities.Tests
@@ -30,6 +31,7 @@ namespace ExcelTableConverter.Utilities.Tests
     [Test]
     public void IsNumeric_PositiveDecimalNumberDE_ReturnsTrue()
     {
+      Thread.CurrentThread.CurrentCulture = CultureInfo.GetCultureInfo("de-DE");
       var result = "1,234".IsNumericCurrentCulture();
       Assert.That(result, Is.EqualTo(true));
     }
@@ -44,6 +46,7 @@ namespace ExcelTableConverter.Utilities.Tests
     [Test]
     public void IsNumeric_PositiveDecimalNumberWithTousandSeperatorDE_ReturnsTrue()
     {
+      Thread.CurrentThread.CurrentCulture = CultureInfo.GetCultureInfo("de-DE");
       var result = "1.234,5".IsNumericCurrentCulture();
       Assert.That(result, Is.EqualTo(true));
     }
@@ -51,7 +54,9 @@ namespace ExcelTableConverter.Utilities.Tests
     [Test]
     public void IsNumeric_WithBlankBetweenTwoNumbers_ReturnsTrue()
     {
-      Assert.That("1 1".IsNumericCurrentCulture(), Is.True);
+      // note that culture "de-AT" thinks that "1 1" is numeric (returns 11.0)
+      Thread.CurrentThread.CurrentCulture = CultureInfo.GetCultureInfo("de-DE");
+      Assert.That("1 1".IsNumericCurrentCulture(), Is.False);
     }
   }
 }
