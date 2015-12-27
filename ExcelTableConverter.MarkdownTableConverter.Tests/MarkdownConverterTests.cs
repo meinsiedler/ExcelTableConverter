f8ead65cef73af80754d7119cc9dc9bdbf5e1074
Michael Einsiedler <meinsiedler@users.noreply.github.com>
2015-12-27 19:57:22 +0100

    added MarkdownTableConverter and MarkdownTableConverter.Tests projects

---
+++ ExcelTableConverter.MarkdownTableConverter.Tests/MarkdownConverterTests.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using NUnit.Framework;
+
+namespace ExcelTableConverter.MarkdownTableConverter.Tests
+{
+  [TestFixture]
+  public class MarkdownConverterTests
+  {
+    [SetUp]
+    public void SetUp()
+    {
+      Thread.CurrentThread.CurrentCulture = CultureInfo.GetCultureInfo("de-DE");
+    }
+
+    [Test]
+    public void ConverterName_ReturnsMarkdown()
+    {
+      Assert.That(new MarkdownConverter().ConverterName, Is.EqualTo("Markdown"));
+    }
+
+    [Test]
+    public void ToString_ReturnsMarkdown()
+    {
+      Assert.That(new MarkdownConverter().ToString(), Is.EqualTo("Markdown"));
+    }
+
+    [Test]
+    public void GetFileExtension_ReturnsMd()
+    {
+      var result = new MarkdownConverter().GetFileExtension();
+      Assert.That(result, Is.EqualTo("md"));
+    }
+  }
+}
---
+++ ExcelTableConverter.MarkdownTableConverter/MarkdownConverter.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using ExcelTableConverter.ExcelContent.Model;
+using ExcelTableConverter.TableConverter;
+
+namespace ExcelTableConverter.MarkdownTableConverter
+{
+  public class MarkdownConverter : BaseTableConverter
+  {
+    public override string ConverterName
+    {
+      get { return "Markdown"; }
+    }
+
+    public override string ToString()
+    {
+      return ConverterName;
+    }
+
+    public override string GetConvertedContent(Table excelTable)
+    {
+      return "Not implemented yet!";
+    }
+
+    public override string GetFileExtension()
+    {
+      return "md";
+    }
+  }
+}
